7258616347389168c9b9f64062b425f254771f7e
John Henry Thompson <jht1493@gmail.com>
2023-02-08 16:36:42 -0500

    mediaPathFullRez

--- p5js_demos/createImg-board/sketch.js
+++ p5js_demos/createImg-board/sketch.js
@@ -53,7 +53,8 @@ function received_gallery(data) {
   for (val of arr) {
     console.log('val', val);
     // let img = createImg( 'https://p5js.org/assets/img/asterisk-01.png', 'the p5 magenta asterisk' );
-    let img = createImg(val.mediaPath, val.authorEmail);
+    let path = val.mediaPathFullRez ?? val.mediaPath;
+    let img = createImg(path, val.authorEmail);
     div.child(img);
   }
 }
