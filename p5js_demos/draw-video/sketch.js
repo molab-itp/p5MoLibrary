6ce0ea80d3262fbe2136850ed54f500a89b11d07
John Henry Thompson <jht1493@gmail.com>
2023-08-15 15:23:47 -0400

    v10 create_myVideo

--- p5js_demos/draw-video/sketch.js
+++ p5js_demos/draw-video/sketch.js
@@ -2,7 +2,7 @@
 // draw-share-multi
 
 let my = {
-  version: 'v9 ',
+  version: 'v10 ',
   galleryKey: 'mo-draw-web-shared',
   max_points: 200,
   nitems: 0,
@@ -12,7 +12,23 @@ let my = {
 };
 
 function setup() {
-  my.canv = createCanvas(393, 600);
+  my.vwidth = 480; // Aspect ratio of video capture
+  my.vheight = 640;
+  my.face = true; // camera face front or back
+
+  // match canvas to video dimensions
+  my.width = my.vwidth;
+  my.height = my.vheight;
+
+  my.x = floor(my.width / 2);
+  my.y = floor(my.height / 2);
+  my.px = my.x;
+  my.py = my.y;
+
+  // my.layer = createGraphics(my.width, my.height);
+
+  // my.canv = createCanvas(393, 600);
+  my.canv = createCanvas(my.width, my.height);
 
   my.canv.mouseReleased(canvas_mouseReleased);
   my.canv.touchEnded(canvas_mouseReleased);
@@ -23,8 +39,6 @@ function setup() {
 
   gallery_onValue();
 
-  strokeWeight(my.brush_size);
-
   my.clearBtn = createButton('Clear').mousePressed(gallery_clear);
   // my.clearBtn.style('font-size:42px');
 
@@ -34,18 +48,32 @@ function setup() {
   createElement('br');
 
   ui_update();
+
+  create_myVideo();
+
+  strokeWeight(my.brush_size);
 }
 
 function draw() {
+  if (!video_ready()) return;
   // window.scrollBy(0, 1);
 
   background(200);
 
+  // image(my.video, 0, 0);
+
+  draw_points();
+}
+
+function draw_points() {
   for (let index = 1; index < my.points.length; index++) {
     let p1 = my.points[index - 1];
     let p2 = my.points[index];
     if (p1.break || p2.break) continue;
     // line(p1.x + random(-2, 2), p1.y + random(-2, 2), p2.x, p2.y);
+    if (p1.c != undefined) {
+      stroke(p1.c);
+    }
     line(p1.x, p1.y, p2.x, p2.y);
   }
 }
@@ -62,7 +90,8 @@ function mouseDragged() {
       }
     }
   }
-  add_item({ x, y });
+  let c = my.video.get(x, y);
+  add_item({ x, y, c });
   // required to prevent touch drag moving canvas on mobile
   return false;
 }
@@ -162,7 +191,23 @@ function ui_span(id, html) {
   span.html(html);
 }
 
+// create the vidoe capture element based on my.facingMode
+function create_myVideo() {
+  let options = { video: { facingMode: my.facingMode } };
+  my.video = createCapture(options);
+  my.video.size(my.vwidth, my.vheight);
+  my.video.hide();
+}
+
+// is the video ready to be displayed
+function video_ready() {
+  return my.video.loadedmetadata && my.video.width > 0 && my.video.height > 0;
+}
+
 // 2023-08-14 jht: Convert to my variable references
+
+// https://editor.p5js.org/jht9629-nyu/sketches/uKHKJ1yqX
+// show-video-jitter
 
 // # --
 // https://firebase.google.com/docs/database/web/read-and-write?hl=en&authuser=0
