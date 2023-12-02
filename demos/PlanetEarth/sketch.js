4aaf09ce317347b965525a8ef5a8192c4a3ec68a
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-12-02 17:52:24 -0500

    angleY

--- demos/PlanetEarth/sketch.js
+++ demos/PlanetEarth/sketch.js
@@ -1,18 +1,22 @@
 // https://editor.p5js.org/jht9629-nyu/sketches/K_xe4i5md
 // PlanetEarth
 
-let angle = 2.8;
+let angleY = 2.8;
 let angleStep = 0.001;
 let angleDir = 1;
+
 let table;
 let aRadius = 200;
+let aDetail = 2;
+let angleX = -0.1;
 
 let earthImg;
 
 let cam;
 
 function preload() {
-  earthImg = loadImage('earth.jpg');
+  earthImg = loadImage('world-large.jpg');
+  // earthImg = loadImage('earth.jpg');
 }
 
 function setup() {
@@ -29,16 +33,21 @@ function draw() {
 
   // cam.pan(0.0001);
 
-  rotateY(angle);
+  rotateX(angleX);
+  rotateY(angleY);
 
   // orbitControl();
-  angle += angleStep * angleDir;
+  angleY += angleStep * angleDir;
 
   lights();
   fill(255);
   noStroke();
   texture(earthImg);
-  sphere(aRadius);
+  sphere(aRadius, 24 * aDetail, 16 * aDetail);
+}
+
+function mousePressed() {
+  angleDir = (angleDir + 1) % 2;
 }
 
 // https://editor.p5js.org/jht9629-nyu/sketches/SJtBwJIcU
