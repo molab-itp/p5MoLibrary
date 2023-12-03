15b0925a9cd166e3d31189d320b8ab9c333309ec
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-12-03 03:40:31 -0500

    PlanetEarth  new p5

--- demos/PlanetEarth/sketch.js
+++ demos/PlanetEarth/sketch.js
@@ -6,28 +6,40 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight);
   my.aRadius = floor(windowHeight * 0.38);
-  my.layer = createGraphics(windowWidth, windowHeight, WEBGL);
+  // my.layer = createGraphics(windowWidth, windowHeight, WEBGL);
+
+  // pixelDensity(1);
+  const skt = (p_) => {
+    p_.setup = () => {
+      my.aCanvas = p_.createCanvas(windowWidth, windowHeight, WEBGL);
+      my.aCanvas.position(0, 0);
+    };
+    p_.draw = () => {
+      p_.rotateX(my.angleX);
+      p_.rotateY(my.angleY);
+
+      my.angleX += my.angleXstep * my.angleXdir;
+      my.angleY += my.angleYstep * my.angleYdir;
+
+      p_.noStroke();
+      p_.lights();
+      p_.texture(my.earthImg);
+      p_.sphere(my.aRadius, 24 * my.aDetail, 16 * my.aDetail);
+    };
+  };
+  my.p_inst = new p5(skt);
+
   nextDir();
 }
 
 function draw() {
   background(0);
 
-  let layer = my.layer;
+  // let layer = my.layer;
 
-  layer.rotateX(my.angleX);
-  layer.rotateY(my.angleY);
-
-  my.angleX += my.angleXstep * my.angleXdir;
-  my.angleY += my.angleYstep * my.angleYdir;
-
-  layer.lights();
-  layer.texture(my.earthImg);
-  layer.sphere(my.aRadius, 24 * my.aDetail, 16 * my.aDetail);
-
-  image(layer, 0, 0);
+  // image(layer, 0, 0);
 }
 
 function mousePressed() {
