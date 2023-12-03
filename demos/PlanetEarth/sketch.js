ef2d860adc93ca2971a124954ef973a8793339e7
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-12-03 02:54:10 -0500

    PlanetEarth/assets

--- demos/PlanetEarth/sketch.js
+++ demos/PlanetEarth/sketch.js
@@ -1,33 +1,8 @@
 // https://editor.p5js.org/jht9629-nyu/sketches/K_xe4i5md
 // PlanetEarth
 
-let my = {
-  angleX: -0.6,
-  angleXstep: 0.001,
-  angleXdir: 1,
-  // angleY: 2.8, // Africa
-
-  angleY: 5.0, // North America
-  angleYstep: 0.001,
-  angleYdir: 0,
-
-  aRadius: 200,
-  aDetail: 4,
-  dirs: [
-    [0, 1],
-    [0, 0],
-    [0, -1],
-    [0, 0],
-    [1, 0],
-    [0, 0],
-    [-1, 0],
-    [0, 0],
-  ],
-  dirIndex: 0,
-};
-
 function preload() {
-  my.earthImg = loadImage('world-ultra.jpg');
+  my.earthImg = loadImage('assets/world-ultra.jpg');
 }
 
 function setup() {
