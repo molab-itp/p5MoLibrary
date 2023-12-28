dbf9c331b6b5bf03c2057112a289b57bc1e588d9
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-12-27 19:57:17 -0500

    ?v=26

--- src/lib/0/a_main.js
+++ src/lib/0/a_main.js
@@ -2,16 +2,16 @@
 
 // console.log('in a_main.js');
 
-import './fb_config.js?v=25';
-import './Anim.js?v=25';
-import './Pane.js?v=25';
-import './Rect.js?v=25';
-import './RefBox.js?v=25';
-import './get_url_params.js?v=25';
-import './video_create.js?v=25';
-import './ui_logDetail.js?v=25';
-import './ui_logSummary.js?v=25';
-import './ui_span.js?v=25';
-import './dtime.js?v=25';
+import './fb_config.js?v=26';
+import './Anim.js?v=26';
+import './Pane.js?v=26';
+import './Rect.js?v=26';
+import './RefBox.js?v=26';
+import './get_url_params.js?v=26';
+import './video_create.js?v=26';
+import './ui_logDetail.js?v=26';
+import './ui_logSummary.js?v=26';
+import './ui_span.js?v=26';
+import './dtime.js?v=26';
 
 // console.log('a_main Anim ', Anim);
--- src/lib/0/fb_config.js
+++ src/lib/0/fb_config.js
@@ -67,9 +67,9 @@ function init(config) {
   return config;
 }
 
-import { fstore } from './fb_fstore.js?v=25';
+import { fstore } from './fb_fstore.js?v=26';
 
-import { fbase } from './fb_fbase.js?v=25';
+import { fbase } from './fb_fbase.js?v=26';
 
 // export api for non-module script
 const fb_ = {
