f0fa6fcf435117647520d56483d83ac3a80eaf36
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-12-31 21:30:15 -0500

    32 lib/0/style.css

--- src/lib/0/a_main.js
+++ src/lib/0/a_main.js
@@ -2,16 +2,16 @@
 
 // console.log('in a_main.js');
 
-import './fb_config.js?v=31';
-import './Anim.js?v=31';
-import './Pane.js?v=31';
-import './Rect.js?v=31';
-import './RefBox.js?v=31';
-import './get_url_params.js?v=31';
-import './video_create.js?v=31';
-import './ui_logDetail.js?v=31';
-import './ui_logSummary.js?v=31';
-import './ui_span.js?v=31';
-import './dtime.js?v=31';
+import './fb_config.js?v=32';
+import './Anim.js?v=32';
+import './Pane.js?v=32';
+import './Rect.js?v=32';
+import './RefBox.js?v=32';
+import './get_url_params.js?v=32';
+import './video_create.js?v=32';
+import './ui_logDetail.js?v=32';
+import './ui_logSummary.js?v=32';
+import './ui_span.js?v=32';
+import './dtime.js?v=32';
 
 // console.log('a_main Anim ', Anim);
