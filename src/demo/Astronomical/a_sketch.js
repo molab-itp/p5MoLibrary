5f768e182eedf36df122fe0fe93d51a9e5cce0b4
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2024-01-09 19:56:20 -0500

    62

--- src/demo/Astronomical/a_sketch.js
+++ src/demo/Astronomical/a_sketch.js
@@ -6,7 +6,7 @@ let my = {};
 
 function preload() {
   //
-  my.version = '?v=61';
+  my.version = '?v=62';
 
   my.backgImg = loadImage('../../assets/The_Celestial_Zoo.png');
 }
--- src/demo/mo-pixel-grid/my_setup.js
+++ src/demo/mo-pixel-grid/my_setup.js
@@ -1,7 +1,7 @@
 //
 function my_setup() {
   // updated to verify change on mobile
-  my.version = '?v=61';
+  my.version = '?v=62';
   // Aspect ratio of video capture
   my.vwidth = 480;
   my.vheight = 640;
