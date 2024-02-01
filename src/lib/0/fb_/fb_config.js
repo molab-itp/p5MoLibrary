b81c65b3a2bd38cf073f76b26774bbe125efc77a
John Henry Thompson <jht9629@nyu.edu>
2024-02-01 12:56:03 -0500

    86

--- src/lib/0/fb_/fb_config.js
+++ src/lib/0/fb_/fb_config.js
@@ -71,9 +71,9 @@ function init(config) {
   return nconfig;
 }
 
-import { fstore } from './fb_fstore.js?v=85';
+import { fstore } from './fb_fstore.js?v=86';
 
-import { fbase } from './fb_fbase.js?v=85';
+import { fbase } from './fb_fbase.js?v=86';
 
 // export api for non-module script
 const fb_ = {
