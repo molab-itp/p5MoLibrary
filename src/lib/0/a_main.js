f0eee2fa983da57f7437f165af49d104690e1611
John Henry Thompson <jht9629@nyu.edu>
2024-02-02 19:00:39 -0500

    130

--- src/lib/0/a_main.js
+++ src/lib/0/a_main.js
@@ -2,15 +2,15 @@
 
 // console.log('in a_main.js');
 
-import './fb_/fb_config.js?v=129';
-import './Anim.js?v=129';
-import './Pane.js?v=129';
-import './Rect.js?v=129';
-import './RefBox.js?v=129';
-import './get_url_params.js?v=129';
-import './video_create.js?v=129';
-import './ui_/ui_logDetail.js?v=129';
-import './ui_/ui_logSummary.js?v=129';
-import './ui_/ui_span.js?v=129';
+import './fb_/fb_config.js?v=130';
+import './Anim.js?v=130';
+import './Pane.js?v=130';
+import './Rect.js?v=130';
+import './RefBox.js?v=130';
+import './get_url_params.js?v=130';
+import './video_create.js?v=130';
+import './ui_/ui_logDetail.js?v=130';
+import './ui_/ui_logSummary.js?v=130';
+import './ui_/ui_span.js?v=130';
 
 // console.log('a_main Anim ', Anim);
