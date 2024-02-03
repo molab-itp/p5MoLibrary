7da4f75da61905f18af55681234a52e075d41329
John Henry Thompson <jht9629@nyu.edu>
2024-02-03 01:50:48 -0500

    dumpDateFacts

--- src/demo/mo-blackfacts/player.js
+++ src/demo/mo-blackfacts/player.js
@@ -10,6 +10,28 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 let dateFactsKeys = Object.keys(dateFacts).sort();
 let nfacts = dateFactsKeys.length;
+for (let index = 0; index < nfacts; index++) {
+  let key = dateFactsKeys[index];
+  dateFacts[key].index = index;
+}
+
+function dumpDateFacts() {
+  let datesJSON = JSON.stringify(dateFacts, null, 2);
+  downloadToFile('dateFacts', datesJSON);
+}
+
+// https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
+function downloadToFile(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+dumpDateFacts();
 
 let videoKey = null;
 let player = null;
