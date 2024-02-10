5c9b5d3dec34141de76542619b75720e67c29d5f
John Henry Thompson <jht9629@nyu.edu>
2024-02-10 05:09:51 -0500

    lib/0 156

--- src/lib/0/a_main.js
+++ src/lib/0/a_main.js
@@ -2,18 +2,15 @@
 
 // console.log('in a_main.js');
 
-import './Anim.js?v=158';
-import './dstore/dstore_device_event.js?v=158';
-import './dstore/dstore_device_onChild.js?v=158';
-import './dstore/dstore_updateTimeGap.js?v=158';
-import './fb_/fb_config.js?v=158';
-import './get_url_params.js?v=158';
-import './Pane.js?v=158';
-import './Rect.js?v=158';
-import './RefBox.js?v=158';
-import './ui_/ui_logDetail.js?v=158';
-import './ui_/ui_logSummary.js?v=158';
-import './ui_/ui_span.js?v=158';
-import './video_create.js?v=158';
+import './fb_/fb_config.js?v=156';
+import './Anim.js?v=156';
+import './Pane.js?v=156';
+import './Rect.js?v=156';
+import './RefBox.js?v=156';
+import './get_url_params.js?v=156';
+import './video_create.js?v=156';
+import './ui_/ui_logDetail.js?v=156';
+import './ui_/ui_logSummary.js?v=156';
+import './ui_/ui_span.js?v=156';
 
 // console.log('a_main Anim ', Anim);
