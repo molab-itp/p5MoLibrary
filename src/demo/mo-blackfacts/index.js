92e66fa067c8756c2891384fa35035e4704225a6
John Henry Thompson <jht9629@nyu.edu>
2024-02-29 16:18:37 -0500

    27

--- src/demo/mo-blackfacts/index.js
+++ src/demo/mo-blackfacts/index.js
@@ -18,7 +18,7 @@ function document_loaded() {
 
   // let config = fireb_.init('jht9629');
   // // let config = fireb_.init('jht1493');
-  // console.log('?v=26 config.projectId', config.projectId);
+  // console.log('?v=27 config.projectId', config.projectId);
   // console.log('configLabel', config.configLabel);
 
   dbase_app_init({ completed: app_init_completed });
@@ -67,7 +67,7 @@ function my_init() {
   my.blackfacts_index = -1;
   my.stepCount = 0;
   my.animTime = 7;
-  my.nameDevice = 'device?v=26';
+  my.nameDevice = 'device?v=27';
   my.pingTime = 1;
 
   // my.isRemote = window.innerHeight > window.innerWidth;
--- src/lib/a_lib.js
+++ src/lib/a_lib.js
@@ -2,23 +2,23 @@
 
 // console.log('in a_main.js');
 
-import './Anim.js?v=26';
-import './dbase/a_devices_observe.js?v=26';
-import './dbase/a_devices_update.js?v=26';
-import './dbase/app_init.js?v=26';
-import './dbase/app_observe.js?v=26';
-import './dbase/device_event.js?v=26';
-import './dbase/device_observe.js?v=26';
-import './dbase/update_props.js?v=26';
-import './dbase/updateTimeGap.js?v=26';
-import './fireb_/fireb_config.js?v=26';
-import './get_url_params.js?v=26';
-import './Pane.js?v=26';
-import './Rect.js?v=26';
-import './RefBox.js?v=26';
-import './ui_/ui_logDetail.js?v=26';
-import './ui_/ui_logSummary.js?v=26';
-import './ui_/ui_span.js?v=26';
-import './video_create.js?v=26';
+import './Anim.js?v=27';
+import './dbase/a_devices_observe.js?v=27';
+import './dbase/a_devices_update.js?v=27';
+import './dbase/app_init.js?v=27';
+import './dbase/app_observe.js?v=27';
+import './dbase/device_event.js?v=27';
+import './dbase/device_observe.js?v=27';
+import './dbase/update_props.js?v=27';
+import './dbase/updateTimeGap.js?v=27';
+import './fireb_/fireb_config.js?v=27';
+import './get_url_params.js?v=27';
+import './Pane.js?v=27';
+import './Rect.js?v=27';
+import './RefBox.js?v=27';
+import './ui_/ui_logDetail.js?v=27';
+import './ui_/ui_logSummary.js?v=27';
+import './ui_/ui_span.js?v=27';
+import './video_create.js?v=27';
 
 // console.log('a_main Anim ', Anim);
