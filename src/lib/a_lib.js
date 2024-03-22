6c482e969bb2e576d542ccdb6743930738077a54
John Henry Thompson <jht9629@nyu.edu>
2024-03-22 17:28:44 -0400

    60

--- src/lib/a_lib.js
+++ src/lib/a_lib.js
@@ -2,14 +2,14 @@
 
 // console.log('in a_main.js');
 
-import './dbase/a_dbase.js?v=59';
-import './get_url_params.js?v=59';
-import './Pane.js?v=59';
-import './Rect.js?v=59';
-import './RefBox.js?v=59';
-import './ui_/ui_logDetail.js?v=59';
-import './ui_/ui_logSummary.js?v=59';
-import './ui_/ui_span.js?v=59';
-import './video_create.js?v=59';
+import './dbase/a_dbase.js?v=60';
+import './get_url_params.js?v=60';
+import './Pane.js?v=60';
+import './Rect.js?v=60';
+import './RefBox.js?v=60';
+import './ui_/ui_logDetail.js?v=60';
+import './ui_/ui_logSummary.js?v=60';
+import './ui_/ui_span.js?v=60';
+import './video_create.js?v=60';
 
 // console.log('a_main Anim ', Anim);
--- src/lib/dbase/a_dbase.js
+++ src/lib/dbase/a_dbase.js
@@ -1,12 +1,12 @@
 //
 
-import './app_init.js?v=59';
-import './app_observe.js?v=59';
-import './devices_observe.js?v=59';
-import './devices_update.js?v=59';
-import './site_event.js?v=59';
-import './site_observe.js?v=59';
-import './update_props.js?v=59';
-import './updateTimeGap.js?v=59';
-import '../Anim.js?v=59';
-import '../fireb_/fireb_config.js?v=59';
+import './app_init.js?v=60';
+import './app_observe.js?v=60';
+import './devices_observe.js?v=60';
+import './devices_update.js?v=60';
+import './site_event.js?v=60';
+import './site_observe.js?v=60';
+import './update_props.js?v=60';
+import './updateTimeGap.js?v=60';
+import '../Anim.js?v=60';
+import '../fireb_/fireb_config.js?v=60';
