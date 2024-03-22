8e481736fc431693493fcc06aa0d21eafa0e1fed
John Henry Thompson <jht9629@nyu.edu>
2024-03-22 17:09:46 -0400

    Update a_lib.js

--- src/lib/a_lib.js
+++ src/lib/a_lib.js
@@ -2,17 +2,7 @@
 
 // console.log('in a_main.js');
 
-// import './Anim.js?v=54';
 import './dbase/a_dbase.js?v=54';
-// import './dbase/app_init.js?v=54';
-// import './dbase/app_observe.js?v=54';
-// import './dbase/devices_observe.js?v=54';
-// import './dbase/devices_update.js?v=54';
-// import './dbase/site_event.js?v=54';
-// import './dbase/site_observe.js?v=54';
-// import './dbase/update_props.js?v=54';
-// import './dbase/updateTimeGap.js?v=54';
-// import './fireb_/fireb_config.js?v=54';
 import './get_url_params.js?v=54';
 import './Pane.js?v=54';
 import './Rect.js?v=54';
