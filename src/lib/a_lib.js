197fe173b5050792645f6638916b8937abde8d4d
John Henry Thompson <jht9629@nyu.edu>
2024-03-22 17:27:05 -0400

    59

--- src/lib/a_lib.js
+++ src/lib/a_lib.js
@@ -2,14 +2,14 @@
 
 // console.log('in a_main.js');
 
-import './dbase/a_dbase.js?v=58';
-import './get_url_params.js?v=58';
-import './Pane.js?v=58';
-import './Rect.js?v=58';
-import './RefBox.js?v=58';
-import './ui_/ui_logDetail.js?v=58';
-import './ui_/ui_logSummary.js?v=58';
-import './ui_/ui_span.js?v=58';
-import './video_create.js?v=58';
+import './dbase/a_dbase.js?v=59';
+import './get_url_params.js?v=59';
+import './Pane.js?v=59';
+import './Rect.js?v=59';
+import './RefBox.js?v=59';
+import './ui_/ui_logDetail.js?v=59';
+import './ui_/ui_logSummary.js?v=59';
+import './ui_/ui_span.js?v=59';
+import './video_create.js?v=59';
 
 // console.log('a_main Anim ', Anim);
