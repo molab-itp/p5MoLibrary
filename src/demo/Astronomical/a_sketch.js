432ae9ce16707919628a379d4b8e97fc1ec23820
John Henry Thompson <jht9629@nyu.edu>
2024-05-07 21:29:33 -0400

    87

--- src/demo/Astronomical/a_sketch.js
+++ src/demo/Astronomical/a_sketch.js
@@ -9,7 +9,7 @@ let my = {};
 
 function preload() {
   //
-  my.version = '?v=86';
+  my.version = '?v=87';
 
   my.backgImg = loadImage('../../assets/The_Celestial_Zoo.png');
 
--- src/lib/dbase/a_dbase.js
+++ src/lib/dbase/a_dbase.js
@@ -1,13 +1,13 @@
 //
 
-import './app_init.js?v=86';
-import './app_observe.js?v=86';
-import './devices_observe.js?v=86';
-import './devices_update.js?v=86';
-import './fstorage_upload.js?v=86';
-import './site_event.js?v=86';
-import './site_observe.js?v=86';
-import './update_props.js?v=86';
-import './updateTimeGap.js?v=86';
-import '../Anim.js?v=86';
-import '../fireb_/fireb_config.js?v=86';
+import './app_init.js?v=87';
+import './app_observe.js?v=87';
+import './devices_observe.js?v=87';
+import './devices_update.js?v=87';
+import './fstorage_upload.js?v=87';
+import './site_event.js?v=87';
+import './site_observe.js?v=87';
+import './update_props.js?v=87';
+import './updateTimeGap.js?v=87';
+import '../Anim.js?v=87';
+import '../fireb_/fireb_config.js?v=87';
