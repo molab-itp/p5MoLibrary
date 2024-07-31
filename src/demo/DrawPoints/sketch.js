f1ccfd342f57eb09ca1b07033feca52221961d49
John Henry Thompson <jht9629@nyu.edu>
2024-07-31 19:30:22 -0400

    88

--- src/demo/DrawPoints/sketch.js
+++ src/demo/DrawPoints/sketch.js
@@ -5,7 +5,7 @@
 let my = {};
 
 function my_setup() {
-  my.version = '?v=87';
+  my.version = '?v=88';
   my.width = windowWidth;
   my.height = windowHeight;
   // my.width = 640;
--- src/demo/mo-astro/a_setup.js
+++ src/demo/mo-astro/a_setup.js
@@ -1,7 +1,7 @@
 //
 function a_setup() {
   //
-  my.version = '?v=87';
+  my.version = '?v=88';
   my.canvas = createCanvas(windowWidth, windowHeight - 90);
   // my.canvas.mousePressed(canvas_mousePressed);
   // my.canvas.mouseReleased(canvas_mouseReleased);
