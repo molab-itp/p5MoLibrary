b54fd4c8efd28387182ecedbf4378a4b1bac1f86
John Henry Thompson <jht9629@nyu.edu>
2024-08-29 22:04:34 -0400

    96

--- src/demo/mo-storage-dashboard/a_sketch.js
+++ src/demo/mo-storage-dashboard/a_sketch.js
@@ -11,7 +11,7 @@
 let my = {};
 function my_setup() {
   // update to verify change on mobile
-  my.version = '?v=95';
+  my.version = '?v=96';
   // Aspect ratio of video capture/
   // will be scaled by rezScale
   my.width = 480;
--- src/lib/dbase/a_dbase.js
+++ src/lib/dbase/a_dbase.js
@@ -1,13 +1,13 @@
 //
 
-import './app_init.js?v=95';
-import './app_observe.js?v=95';
-import './devices_observe.js?v=95';
-import './devices_update.js?v=95';
-import './fstorage_upload.js?v=95';
-import './site_event.js?v=95';
-import './site_observe.js?v=95';
-import './update_props.js?v=95';
-import './updateTimeGap.js?v=95';
-import '../Anim.js?v=95';
-import '../fireb_/fireb_config.js?v=95';
+import './app_init.js?v=96';
+import './app_observe.js?v=96';
+import './devices_observe.js?v=96';
+import './devices_update.js?v=96';
+import './fstorage_upload.js?v=96';
+import './site_event.js?v=96';
+import './site_observe.js?v=96';
+import './update_props.js?v=96';
+import './updateTimeGap.js?v=96';
+import '../Anim.js?v=96';
+import '../fireb_/fireb_config.js?v=96';
